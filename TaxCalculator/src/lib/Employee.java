bf74f604e5ef9257696153ce48c68ad82dce6a94
zivana afra yulianto <afrazivana5@gmail.com>
2025-04-17 12:33:37 +0700

    Perbaiki penetapan ID pasangan

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -77,7 +77,7 @@ public class Employee {
 	
 	public void setSpouse(String spouseName, String spouseIdNumber) {
 		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
+		this.spouseIdNumber = spouseIdNumber; // Diperbaiki
 	}
 	
 	public void addChild(String childName, String childIdNumber) {
